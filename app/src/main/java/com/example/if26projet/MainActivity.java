cb8bcafeabdf307672cb7b0349261f63ace7ee35
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-10-27 22:39:00 +0100

    Simple textview for plats Simple simple menu

--- app/src/main/java/com/example/if26projet/MainActivity.java
+++ app/src/main/java/com/example/if26projet/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.if26projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Plat> plats = new ArrayList<Plat>();
+        PlatAdapter adapter = new PlatAdapter(
+                this,
+                R.layout.plat,
+                plats
+        );
+        ListView lv = (ListView) findViewById(R.id.menu_lv);
+        lv.setAdapter(adapter);
     }
 }
